a5364361d396f4e0c0d3e7d9f2fa8cd896f2a050
yuta <yu.code.tech@gmail.com>
2023-02-23 19:43:09 +0900

    jsのリファクタリング

--- js/complete.js
+++ js/complete.js
@@ -17,13 +17,13 @@ var attack = [
 // 攻撃ボタンが押された時の処理
 //=============================
 jQuery("#js-attack-btn").click(function () {
-  //2秒間ボタン操作無効
+  //1秒間ボタン操作無効
   //------------
   var $this = jQuery(this);
   $this.css("pointer-events", "none"); //攻撃ボタンのクリックイベントを無効
   setTimeout(function () {
     $this.css("pointer-events", ""); //2秒後に攻撃ボタンのクリックイベント無効を解除
-  }, 2000);
+  }, 1000);
 
   //攻撃アニメーションの処理
   //------------
@@ -40,15 +40,18 @@ jQuery("#js-attack-btn").click(function () {
   attackName = attackObject["name"]; //攻撃名を変数に格納
   attackPoint = attackObject["points"]; //攻撃ダメージを変数に格納
 
-  //ダメージ計算と攻撃イベント
+  //ダメージ計算
   //------------
   monsterHp = monsterHp - attackPoint; //ダメージ計算 [モンスターのHP = モンスターのHP - 攻撃ダメージ]
+
+  //攻撃メッセージと倒した時のイベント
+  //------------
+  jQuery("#js-message").text(`${attackName} / ${attackPoint}ダメージ`); //攻撃名と攻撃ポイントをメッセージを表示
   if (monsterHp <= 0) {
     //もしモンスターのHPが0以下の時
-    monsterHpPercent = 0; //モンスターのHPの%は0
+    monsterHpPercent = 0; //モンスターのHPの%を0
     jQuery("#js-monster,#js-attack-btn").fadeOut(); //モンスターと攻撃ボタンを非表示
     jQuery("#js-message")
-      .text(`${attackName} / ${attackPoint}ダメージ`) //攻撃名と攻撃ポイントをメッセージを表示
       .delay(1000) //1秒後に
       .queue(function () {
         jQuery(this).text("敵を倒しました！！").dequeue(); //メッセージを表示
@@ -57,7 +60,6 @@ jQuery("#js-attack-btn").click(function () {
     //もしモンスターのHPが0以下ではない時は
     monsterHpPercent = monsterHp / monsterMaxHp; //モンスターのHPの%計算 [モンスターのHPの% = モンスターのHP / モンスターの最大HP]
     jQuery("#js-message")
-      .text(`${attackName} / ${attackPoint}ダメージ`) //攻撃名と攻撃ポイントをメッセージを表示
       .delay(1000) //1秒後に
       .queue(function () {
         jQuery(this).text("　").dequeue(); //メッセージを非表示
@@ -66,12 +68,9 @@ jQuery("#js-attack-btn").click(function () {
 
   //HPゲージの処理
   //------------
+  document.querySelector("#js-monster-hp").style.width = monsterHpPercent * 100 + "%"; //HPゲージのwidthをcssで変更
   if (monsterHpPercent <= 0.3) {
     //もしモンスターのHPが30%以下のときは
-    document.querySelector("#js-monster-hp").style.width = monsterHpPercent * 100 + "%"; //HPゲージのwidthをcssで変更
     document.querySelector("#js-monster-hp").style.backgroundColor = "red"; //HPゲージのbackground-colorをcssで変更
-  } else {
-    //もしモンスターのHPが30%以下ではないときは
-    document.querySelector("#js-monster-hp").style.width = monsterHpPercent * 100 + "%"; //HPゲージのwidthをcssで変更
   }
 });
